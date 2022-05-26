79a025a28531655bd4bdaaa7d0c99d007d394a0b
chilarai <chilly5476@gmail.com>
2022-05-26 11:06:42 +0530

    Code refactor

---
+++ src/sources/authentication/github.go
@@ -0,0 +1,155 @@
+package authentication
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+
+func GithubLoggedinHandler(w http.ResponseWriter, r *http.Request, githubData string) {
+	if githubData == "" {
+		// Unauthorized users get an unauthorized message
+		fmt.Fprintf(w, "UNAUTHORIZED!")
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+
+	// Prettifying the json
+	var prettyJSON bytes.Buffer
+	// json.indent is a library utility function to prettify JSON indentation
+	parserr := json.Indent(&prettyJSON, []byte(githubData), "", "\t")
+	if parserr != nil {
+		log.Panic("JSON parse error")
+	}
+
+	// Return the prettified JSON as a string
+	fmt.Fprintf(w, string(prettyJSON.Bytes()))
+}
+
+func GithubContributorLoginHandler(w http.ResponseWriter, r *http.Request) {
+	githubClientID := GetGithubClientID()
+	githubContributorRedirectUri := GetGithubContributorRedirectURI()
+
+	redirectURL := fmt.Sprintf("https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s", githubClientID, githubContributorRedirectUri)
+
+	http.Redirect(w, r, redirectURL, 301)
+}
+
+func GithubProjectLoginHandler(w http.ResponseWriter, r *http.Request) {
+	githubClientID := GetGithubClientID()
+	githubProjectRedirectUri := GetGithubProjectRedirectURI()
+
+	redirectURL := fmt.Sprintf("https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s", githubClientID, githubProjectRedirectUri)
+
+	http.Redirect(w, r, redirectURL, 301)
+}
+
+func GithubCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+
+	githubAccessToken := GetGithubAccessToken(code)
+
+	githubData := GetGithubData(githubAccessToken)
+
+	GithubLoggedinHandler(w, r, githubData)
+}
+
+func GetGithubData(accessToken string) string {
+	req, reqerr := http.NewRequest("GET", "https://api.github.com/user", nil)
+	if reqerr != nil {
+		log.Panic("API Request creation failed")
+	}
+
+	authorizationHeaderValue := fmt.Sprintf("token %s", accessToken)
+	req.Header.Set("Authorization", authorizationHeaderValue)
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	return string(respbody)
+}
+
+func GetGithubAccessToken(code string) string {
+
+	clientID := GetGithubClientID()
+	clientSecret := GetGithubClientSecret()
+
+	requestBodyMap := map[string]string{"client_id": clientID, "client_secret": clientSecret, "code": code}
+	requestJSON, _ := json.Marshal(requestBodyMap)
+
+	req, reqerr := http.NewRequest("POST", "https://github.com/login/oauth/access_token", bytes.NewBuffer(requestJSON))
+	if reqerr != nil {
+		log.Panic("Request creation failed")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	// Represents the response received from Github
+	type githubAccessTokenResponse struct {
+		AccessToken string `json:"access_token"`
+		TokenType   string `json:"token_type"`
+		Scope       string `json:"scope"`
+	}
+
+	var ghresp githubAccessTokenResponse
+	json.Unmarshal(respbody, &ghresp)
+
+	return ghresp.AccessToken
+}
+
+func GetGithubClientID() string {
+
+	githubClientID, exists := os.LookupEnv("GITHUB_CLIENT_ID")
+	if !exists {
+		log.Fatal("Github Client ID not defined in .env file")
+	}
+
+	return githubClientID
+}
+
+func GetGithubClientSecret() string {
+
+	githubClientSecret, exists := os.LookupEnv("GITHUB_CLIENT_SECRET")
+	if !exists {
+		log.Fatal("Github Client ID not defined in .env file")
+	}
+
+	return githubClientSecret
+}
+
+func GetGithubContributorRedirectURI() string {
+
+	githubContributorRedirectURI, exists := os.LookupEnv("GITHUB_CONTRIBUTOR_REDIRECT_URI")
+	if !exists {
+		log.Fatal("Github Contributor Redirect URI not defined in .env file")
+	}
+
+	return githubContributorRedirectURI
+}
+
+func GetGithubProjectRedirectURI() string {
+
+	githubProjectRedirectURI, exists := os.LookupEnv("GITHUB_PROJECT_REDIRECT_URI")
+	if !exists {
+		log.Fatal("Github Project Redirect URI not defined in .env file")
+	}
+
+	return githubProjectRedirectURI
+}
