8c12f7752619bbb1c2c12ae46867450d7404c79e
chilarai <chilly5476@gmail.com>
2022-05-28 11:46:33 +0530

    Submit contributor preferences

--- src/sources/templates/contributors/preferences.go
+++ src/sources/templates/contributors/preferences.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"sources/common"
 )
@@ -13,31 +14,51 @@ import (
 type ContributorPreferencesStruct struct{
 	Languages []string `json:"languages"`
 	NotificationFrequency string `json:"notificationFrequency"`
+	ProjectType []string `json:"projectType"`
+	ContributorCount string `json:"contributorCount"`
+	PaidJob string `json:"paidJob"`
+	Relocation string `json:"relocation"`
+	Qualification string `json:"qualification"`
 }
 
 func ContributorPreferences(w http.ResponseWriter, r *http.Request){
-	tmpl := template.Must(template.ParseFiles("../templates/app/contributors/preferences.html"))
-    tmpl.Execute(w, nil)
-
-	if r.Method == "POST"{
-
-		r.ParseForm()
-		languages := r.Form["language"]
-		notificationFrequency := r.Form.Get("emailFrequency")
-
-		result := ContributorPreferencesStruct{languages, notificationFrequency}
-
-		client := config.Mongoconnect()
-		defer client.Disconnect(context.TODO())
-
-		dbName := common.GetMoDb()
-		saveContributorPreference := client.Database(dbName).Collection(common.CONST_MO_CONTRIBUTOR_PREFERENCES)
-
-		_, err := saveContributorPreference.InsertOne(context.TODO(), result)
-
-		if err != nil {
-			fmt.Println(err)
+	
+	if r.Method == "GET"{
+		tmpl := template.Must(template.ParseFiles("../templates/app/contributors/preferences.html"))
+		tmpl.Execute(w, nil) 
+	} else {
+	
+		errParse := r.ParseForm()
+		if errParse != nil {
+			log.Println(errParse.Error())
+		} else {
+			languages := r.Form["language"]
+			notificationFrequency := r.Form.Get("emailFrequency")
+			projectType := r.Form["pType"]
+			contributorCount := r.Form.Get("contributorCount")
+			paidJob :=  r.Form.Get("paidJob")
+			relocation := r.Form.Get("relocation")
+			qualification := r.Form.Get("qualification")
+	
+			result := ContributorPreferencesStruct{languages, notificationFrequency, projectType, contributorCount, paidJob, relocation, qualification}
+	
+			client := config.Mongoconnect()
+			defer client.Disconnect(context.TODO())
+	
+			dbName := common.GetMoDb()
+			saveContributorPreference := client.Database(dbName).Collection(common.CONST_MO_CONTRIBUTOR_PREFERENCES)
+	
+			_, err := saveContributorPreference.InsertOne(context.TODO(), result)
+	
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
+
+		
+
+		tmpl := template.Must(template.ParseFiles("../templates/app/contributors/preferences.html"))
+		tmpl.Execute(w, nil) 
 
 	}
 }
